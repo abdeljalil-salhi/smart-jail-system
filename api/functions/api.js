4c12ad08896d61cfbb6c01962b7bfe5f11d9987e
Abdeljalil Salhi <abdousalhi868@gmail.com>
2023-05-27 11:28:05 +0100

    Rebase to RestAPI

--- api/functions/api.js
+++
@@ -1,4 +0,0 @@
-require("dotenv").config();
-const { handler } = require("./src/utils/routes");
-
-exports.handler = handler;
--- api/functions/src/controllers/index.js
+++
@@ -1,7 +0,0 @@
-const alarmsController = require("./alarms-crud");
-const prisonersController = require("./prisoners-crud");
-
-module.exports = {
-  alarmsController,
-  prisonersController,
-};
