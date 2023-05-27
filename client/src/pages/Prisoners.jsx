d29779c8a9c393a8dc0115bed8b2ef60edf4a5a7
Abdeljalil Salhi <abdousalhi868@gmail.com>
2023-05-27 11:46:23 +0100

    Fix API_URL not defined in Prisoners component

--- client/src/pages/Prisoners.jsx
+++ client/src/pages/Prisoners.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
+import { API_URL } from "../constants";
+
 const Prisoners = () => {
   const [prisoners, setPrisoners] = useState([]);
 
